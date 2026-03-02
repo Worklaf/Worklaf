98335f3c9678b82b16060ff9e28ee1c0f5f5feba
Worklaf <nikolfar91@gmail.com>
2026-03-02 16:44:11 +0100

    Update languages.js

--- AirdropLab/languages.js
+++ AirdropLab/languages.js
@@ -451,22 +451,22 @@ function updateFeedbackModalTranslations() {
   }
 }
 
+// Обновляем отображение кнопки языка
 function updateLanguageButton() {
   const langBtn = document.getElementById('langBtn');
   if (!langBtn) return;
   
-  // Всегда показываем британский флаг
+  // Британский флаг - более реалистичный SVG
   const ukFlagSVG = `
-    <svg viewBox="0 0 60 45" xmlns="http://www.w3.org/2000/svg" width="32" height="22" class="flag-svg">
-      <rect width="60" height="45" fill="#012169"/>
-      <rect width="60" height="15" fill="#fff"/>
-      <rect width="60" height="45" fill="none" stroke="#fff" stroke-width="6"/>
-      <rect width="60" height="45" fill="none" stroke="#C8102E" stroke-width="4"/>
-      <rect width="20" height="15" fill="#fff"/>
-      <rect x="40" width="20" height="15" fill="#fff"/>
-      <rect y="22.5" width="60" height="15" fill="#fff"/>
-      <rect x="8" y="30" width="12" height="15" fill="#fff"/>
-      <rect x="40" y="30" width="12" height="15" fill="#fff"/>
+    <svg viewBox="0 0 60 30" xmlns="http://www.w3.org/2000/svg" width="36" height="18" style="border-radius:2px;">
+      <clipPath id="t">
+        <path d="M30,15 h30 v15 z v15 h-30 z h-30 v-15 z v-15 h30 z"/>
+      </clipPath>
+      <path d="M0,0 v30 h60 v-30 z" fill="#012169"/>
+      <path d="M0,0 L60,30 M60,0 L0,30" stroke="#fff" stroke-width="6"/>
+      <path d="M0,0 L60,30 M60,0 L0,30" clip-path="url(#t)" stroke="#C8102E" stroke-width="4"/>
+      <path d="M30,0 v30 M0,15 h60" stroke="#fff" stroke-width="10"/>
+      <path d="M30,0 v30 M0,15 h60" stroke="#C8102E" stroke-width="6"/>
     </svg>
   `;
   
